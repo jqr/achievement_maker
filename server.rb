19ce3f6792ba9431b29401c5cb30f34b29137fe3
Elijah Miller <elijah.miller@gmail.com>
2011-07-31 11:48:50 -0400

    Added a homepage.

--- server.rb
+++ server.rb
@@ -4,8 +4,13 @@ Bundler.setup(:default)
 
 require './achievement'
 require 'sinatra'
+require 'erb'
 
 get "/xbox/:text" do
   content_type 'image/png'
   achievement(params[:header] || 'ACHIEVEMENT ULOCKED', params[:text]).to_blob
+end
+
+get "/" do
+  erb :index
 end
