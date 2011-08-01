2ab87ccb650fa768ffde85436a8dae4fd55e74fb
Chris <christopher@fastestforward.com>
2011-08-01 15:15:25 -0400

    add method to mask a gravatar image over the achievement image

--- achievement.rb
+++ achievement.rb
@@ -1,6 +1,14 @@
+require 'digest/md5'
+require 'faraday'
 require 'RMagick'
 
-def achievement(first_line, second_line)
+def gravatar_image(email, dims = 46)
+  client = Faraday::Connection.new(:url => "http://www.gravatar.com/")  
+  hash = Digest::MD5.hexdigest(email.downcase.strip)
+  client.get("/avatar/#{hash}?s=#{dims}&d=mm").body
+end
+
+def achievement(first_line, second_line, gravatar = nil)
   canvas = Magick::Image.new(423, 67) { |c| c.background_color = "none"; c.format = "png" }
   draw = Magick::Draw.new
 
@@ -8,8 +16,7 @@ def achievement(first_line, second_line)
   draw.circle(33,33, 33,66)
   draw.circle(423 - 34,33, 423 - 34,66)
   draw.rectangle(33, 0, 423 - 34,67)
-
-
+  
   draw.fill("#1B1D1A")
   draw.circle(33,33, 33,62)
 
@@ -17,9 +24,30 @@ def achievement(first_line, second_line)
   draw.circle(33,33, 33,60)
 
   draw.fill("#1B1D1A")
+  
   draw.rectangle(30, 6, 36,60)
   draw.rectangle(6, 30, 60,36)
   draw.circle(33,33, 33,54)
+
+  if gravatar && (avatar = gravatar_image(gravatar))
+    odraw = Magick::Draw.new
+    mdraw = Magick::Draw.new 
+    aimg = Magick::Image.from_blob(avatar).first
+    overlay = Magick::Image.new(423, 67)
+    mask = Magick::Image.new(423, 67)
+
+    odraw.composite(6, 8, 54, 54, aimg) 
+    odraw.draw(overlay) 
+    mdraw.fill("rgba(0,0,0,255)")
+    mdraw.rectangle(0, 0, 423,67)
+    mdraw.fill("rgba(255,255,255,255)")
+    mdraw.circle(33, 33, 33, 54)
+    mdraw.draw(mask)
+    mask.matte = false
+    overlay.matte = true 
+    overlay.composite!(mask, 0, 0, Magick::CopyOpacityCompositeOp)
+    draw.composite(0, 0, 423, 67, overlay)
+  end
 
   draw.fill("#FFFFFF")
 
