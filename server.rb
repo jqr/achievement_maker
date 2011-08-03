b9be8e43a3c70a0e3ef53efff9030f6e783fe4a0
Elijah Miller <elijah.miller@gmail.com>
2011-08-03 08:08:41 -0500

    Stripping image extensions from all parameters for Propane/Campfire paste compatability.

--- server.rb
+++ server.rb
@@ -11,7 +11,10 @@ require 'erb'
 get "/xbox/:text" do
   content_type 'image/png'
   response['Cache-Control'] = "public, max-age=#{60*24*7}" # cache for one week
-  achievement(params[:header] || 'ACHIEVEMENT UNLOCKED', params[:text], params[:email]).to_blob
+  achievement = params[:text].to_s.sub(/\.(jpeg|jpg|png|gif)$/i, '')
+  header = (params[:header] || 'ACHIEVEMENT UNLOCKED').to_s.sub(/\.(jpeg|jpg|png|gif)$/i, '')
+  email = params[:email].to_s.sub(/\.(jpeg|jpg|png|gif)$/i, '')
+  achievement(header, achievement, email).to_blob
 end
 
 get "/" do
