5b5f33a6c673131b50b77a8866a04aaf7f6fd317
Elijah Miller <elijah.miller@gmail.com>
2024-09-28 13:30:26 -0400

    Player highlighting and numerous other improvements.

--- achievement.rb
+++ achievement.rb
@@ -16,41 +16,54 @@ class Achievement
   FIRST_LINE_KERNING  = "0.65"
   SECOND_LINE_KERNING = "0.4"
 
-  attr_accessor :first_line, :second_line, :email
+  attr_reader :first_line, :second_line, :email, :player
 
-  def initialize(achievement:, header: "ACHIEVEMENT UNLOCKED", email: nil)
+  attr_reader :width, :height, :scale, :format
+  attr_reader :draw
+
+  def initialize(achievement:, header: "ACHIEVEMENT UNLOCKED", email: nil, player: 1, width: 423, height: 67, scale: 1, format: "png")
     @first_line = header
     @second_line = achievement
     @email = email
+    @player = player
+
+    @scale = scale
+    @width = width * scale
+    @height = height * scale
+    @format = format
   end
 
   def draw_circle(draw, x, y, radius)
     draw.circle(x, y, x, y + radius)
   end
 
-  def draw_rounded_area(draw, width, height)
-    radius = height / 2
+  def draw_rounded_area
     draw_circle(draw, radius, radius, radius)
     draw_circle(draw, width - radius - 1, radius, radius)
     draw.rectangle(radius, 0, width - radius - 1, height)
   end
 
-  def render(width:, height:, scale:)
-    draw = Magick::Draw.new
+  def radius
+    height / 2
+  end
 
-    draw.fill(BACKGROUND)
-    draw_rounded_area(draw, width, height)
+  def dark_circle_radius
+    radius - 4 * scale
+  end
 
-    radius = height / 2
-    dark_circle_radius = radius - 4 * scale
+  def player_circle_outer_radius
+    dark_circle_radius - 2 * scale
+  end
 
-    draw.fill(CIRCLE)
-    draw_circle(draw, radius, radius, dark_circle_radius)
+  def player_circle_highlight_outer_radius
+    player_circle_outer_radius - 1
+  end
 
-    player_circle_outer_radius = dark_circle_radius - 2 * scale
-    draw.fill(CIRLCE_OFF)
-    draw_circle(draw, radius, radius, player_circle_outer_radius)
+  def crosshair_width
+    6 * scale
+  end
 
+  def draw_player_highlight
     odraw = Magick::Draw.new
     mdraw = Magick::Draw.new
     overlay = Magick::Image.new(width, height)
@@ -63,12 +76,25 @@ class Achievement
     mdraw.rectangle(0, 0, width, height)
     mdraw.fill(FULL_OPACITY)
 
-    player_circle_highlight_outer_radius = player_circle_outer_radius - 1
     draw_circle(mdraw, radius, radius, player_circle_highlight_outer_radius)
 
     mdraw.fill(TRANSPARENT)
-    mdraw.rectangle(radius, 0, width, height)
-    mdraw.rectangle(0, radius, radius, height)
+    case player
+    when 1
+      mdraw.rectangle(radius, 0, width, height)
+      mdraw.rectangle(0, radius, radius, height)
+    when 2
+      mdraw.rectangle(radius, radius, width, height)
+      mdraw.rectangle(0, 0, radius, height)
+    when 3
+      mdraw.rectangle(0, 0, width, height)
+      mdraw.rectangle(0, radius, radius, height)
+    when 4
+      mdraw.rectangle(0, radius, radius, height)
+      mdraw.rectangle(0, 0, width, radius)
+    else
+      mdraw.rectangle(0, 0, width, height)
+    end
 
     mdraw.draw(mask)
     mask.alpha(Magick::OffAlphaChannel)
@@ -76,36 +102,68 @@ class Achievement
 
     overlay.composite!(mask, 0, 0, Magick::CopyAlphaCompositeOp)
     draw.composite(0, 0, width, height, overlay)
+  end
 
-    # Crosshair
+  def draw_player_circle
+    draw.fill(CIRCLE)
+    dark_circle_radius = radius - 4 * scale
+    draw_circle(@draw, radius, radius, dark_circle_radius)
+
+    player_circle_outer_radius = dark_circle_radius - 2 * scale
+    draw.fill(CIRLCE_OFF)
+    draw_circle(draw, radius, radius, player_circle_outer_radius)
+
+    draw_player_highlight
+    draw_crosshair
+  end
+
+  def draw_crosshair
     draw.fill(CIRCLE_CENTER)
-    crosshair_width = 6 * scale
     draw.rectangle(radius - crosshair_width / 2, radius - player_circle_highlight_outer_radius - 1, radius + crosshair_width / 2, radius * 2 - crosshair_width)
     draw.rectangle(radius - player_circle_highlight_outer_radius - 1, radius - crosshair_width / 2, radius * 2 - crosshair_width, radius + crosshair_width / 2)
     player_circle_highlight_inner_radius = player_circle_outer_radius - 7 * scale
     draw_circle(draw, radius, radius, player_circle_highlight_inner_radius)
+  end
 
-    if email.to_s.size > 1 && (avatar = Gravatar.new(email).image_data)
-      odraw = Magick::Draw.new
-      mdraw = Magick::Draw.new
-      aimg = Magick::Image.from_blob(avatar).first
-      overlay = Magick::Image.new(width, height)
-      mask = Magick::Image.new(width, height)
+  def email?
+    email.to_s.size > 1
+  end
 
-      odraw.composite(radius - player_circle_highlight_outer_radius - 1, radius - player_circle_highlight_outer_radius - 1, 54*scale, 54*scale, aimg)
-      odraw.draw(overlay)
-      mdraw.fill(TRANSPARENT)
-      mdraw.rectangle(0, 0, width, height)
-      mdraw.fill(FULL_OPACITY)
-      draw_circle(mdraw, radius, radius, 16 * scale)
-      mdraw.draw(mask)
-      mask.alpha(Magick::OffAlphaChannel)
-      overlay.alpha(Magick::OnAlphaChannel)
+  def avatar
+    @avatar ||= Gravatar.new(email).image_data if email?
+  end
 
-      overlay.composite!(mask, 0, 0, Magick::CopyAlphaCompositeOp)
-      draw.composite(0, 0, width, height, overlay)
-    end
+  def avatar?
+    !!avatar
+  end
 
+  def draw_avatar
+    odraw = Magick::Draw.new
+    mdraw = Magick::Draw.new
+    aimg = Magick::Image.from_blob(avatar).first
+    overlay = Magick::Image.new(width, height)
+    mask = Magick::Image.new(width, height)
+
+    odraw.composite(radius - player_circle_highlight_outer_radius - 1, radius - player_circle_highlight_outer_radius - 1, 54*scale, 54*scale, aimg)
+    odraw.draw(overlay)
+    mdraw.fill(TRANSPARENT)
+    mdraw.rectangle(0, 0, width, height)
+    mdraw.fill(FULL_OPACITY)
+    draw_circle(mdraw, radius, radius, 16 * scale)
+    mdraw.draw(mask)
+    mask.alpha(Magick::OffAlphaChannel)
+    overlay.alpha(Magick::OnAlphaChannel)
+
+    overlay.composite!(mask, 0, 0, Magick::CopyAlphaCompositeOp)
+    draw.composite(0, 0, width, height, overlay)
+  end
+
+  def draw_player_area
+    draw_player_circle
+    draw_avatar if avatar?
+  end
+
+  def draw_text
     draw.fill(WHITE)
 
     draw.font(FONT)
@@ -116,26 +174,32 @@ class Achievement
     draw.font_size(FONT_SIZE.to_f * scale)
     draw.kerning(SECOND_LINE_KERNING)
     draw.text(75 * scale, 50 * scale, second_line)
-
-    draw
   end
 
+  def render
+    @draw = Magick::Draw.new
 
-  def to_blob(format: "png", scale: 1)
-    width = 423 * scale
-    height = 67 * scale
+    @draw.fill(BACKGROUND)
 
+    draw_rounded_area
+    draw_player_area
+    draw_text
+
+    @draw
+  end
+
+  def format_supports_transparent?
+    !(format == "jpeg" || format == "jpg")
+  end
+
+  def to_blob
     canvas = Magick::Image.new(width, height) do |c|
-      if format == "jpeg" || format == "jpg"
-        c.background_color = "#FFFFFF"
-      else
-        c.background_color = "none"
-      end
+      c.background_color = format_supports_transparent? ? "none" : "#FFFFFF"
       c.format = format
       c.density = 72 * scale
     end
 
-    render(width:, height:, scale:).draw(canvas)
+    render.draw(canvas)
     canvas.to_blob
   end
 end
--- server.rb
+++ server.rb
@@ -14,13 +14,14 @@ get "/xbox/:text" do
   achievement = remove_image_suffix(params[:text])
   header = remove_image_suffix(params[:header] || "ACHIEVEMENT UNLOCKED")
   email = remove_image_suffix(params[:email])
+  player = remove_image_suffix(params[:player] || "1").to_i
 
   if env == "production"
     response["Cache-Control"] = "public, max-age=#{60*24*30}" # 30 day cache
   end
 
   content_type "image/png"
-  Achievement.new(achievement:, header:, email:).to_blob
+  Achievement.new(achievement:, header:, email:, player:).to_blob
 end
 
 get "/" do
